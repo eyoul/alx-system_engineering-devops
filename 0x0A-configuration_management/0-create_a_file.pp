7d26f41cc47618402cdb03789c3f14007fceda36
eyoul <eyoulshimeles2@gmail.com>
2022-12-09 13:29:11 -0800

    First puppet-lint file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# To Create a /tmp file
+
+file { 'school':
+	ensure => 'present',
+	content => 'I love Puppet',
+	group => 'www-data',
+	mode => '0744',
+	owener => 'www-data'
+	path => '/tmp/school',
+}
