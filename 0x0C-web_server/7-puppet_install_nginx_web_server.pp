91a72e3bafcb5371e6c8e4bbca1e12494732e1d3
eyoul <eyoulshimeles2@gmail.com>
2022-12-13 10:33:31 -0800

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Puppet manifest to install nginx
 package { 'nginx':
   ensure => installed,
@@ -7,7 +8,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://youtu.be/NP8M9AIB3CE;',
 }
 
 file { '/var/www/html/index.html':
@@ -18,8 +19,3 @@ service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-
