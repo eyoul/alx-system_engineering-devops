6e7fbf2d2c52288771282dde22fbadc8ad9ecca4
Eyoul S <49184150+eyoul@users.noreply.github.com>
2023-02-01 01:18:04 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,7 +3,7 @@
 from requests import get
 
 REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'esw1229/0.0.1'}
+HEADERS = {"linux:0x16.api.advanced:v1.0.0 (by /u/esw1229)"}
 
 
 def count_words(subreddit, word_list, after="", word_dic={}):
